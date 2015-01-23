5b71ed9a67330f2217d633cbe5ea140656f0962f
ryuta_note_ubuntu <ryuta0221@gmail.com>
2015-01-24 02:13:26 +0900

    mainにng-repeat追加してみた

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,40 @@
  * # MainCtrl
  * Controller of the yoAngularApp
  */
+
+var MainController = function() {
+  this.controllre = function($scope) {
+    // this.ws = [];
+    // $scope.ws = ws;
+
+    $scope.work = function() {
+      alert('push');
+    };
+  };
+};
+
+function myCont($scope){
+  var ws = [1];
+  var ts = [];
+  $scope.ws = ws;
+  $scope.ts = ts;
+
+  $scope.work = function(){
+    if(ws.length == 1){
+      ws = []
+      $scope.ws = ws;
+
+      ts = [1];
+      $scope.ts = ts;
+
+    } else {
+      ts = [];
+      $scope.ts = ts;
+      ws.push(1)
+      $scope.ws = ws;
+    }
+  };
+}
+
 angular.module('yoAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', myCont);
