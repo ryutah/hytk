f163b277b6acab740bded3ccccd47fc3d5dbc3da
ryuta <ryuta0221@gmail.com>
2015-01-24 14:27:39 +0900

    タブアニメーション追加

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,39 +8,19 @@
  * Controller of the yoAngularApp
  */
 
-var MainController = function() {
-  this.controllre = function($scope) {
-    // this.ws = [];
-    // $scope.ws = ws;
+var MainController = {
+  controller: function($scope) {
+    var work = true;
+    var blog = false;
 
     $scope.work = function() {
-      alert('push');
+      work = !work;
+      blog = !blog;
+      $scope.main.work = work;
+      $scope.main.blog = blog;
     };
-  };
+  }
 };
 
-function myCont($scope){
-  var ws = [1];
-  var ts = [];
-  $scope.ws = ws;
-  $scope.ts = ts;
-
-  $scope.work = function(){
-    if(ws.length == 1){
-      ws = []
-      $scope.ws = ws;
-
-      ts = [1];
-      $scope.ts = ts;
-
-    } else {
-      ts = [];
-      $scope.ts = ts;
-      ws.push(1)
-      $scope.ws = ws;
-    }
-  };
-}
-
 angular.module('yoAngularApp')
-  .controller('MainCtrl', myCont);
+  .controller('MainCtrl', MainController.controller);
