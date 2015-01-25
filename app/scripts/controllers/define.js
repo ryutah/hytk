efd78fcc58c6e1c1622d890f095f32b83dd3fa0b
ryta note <ryuta0221@gmail.com>
2015-01-25 21:24:31 +0900

    ajax test and any fix

---
+++ app/scripts/controllers/define.js
@@ -0,0 +1,7 @@
+var Define = {
+  Top : '',
+  BlogList : '',
+  BlogContent : '',
+  WorkList : '',
+  WorkContent : ''
+};
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,43 @@
  * Controller of the yoAngularApp
  */
 
-function MainCtrl() {
+var Content = function(title, img) {
+  this.title = title;
+  this.img = img;
+};
+
+function MainCtrl($http) {
+  // console.log($http);
+
+  var url = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139';
+
+  $http({
+    url : url,
+    method : 'get'
+  })
+  .success(function(data, status, headers, config) {
+    console.log('data:' + JSON.stringify(data));
+    console.log('status:' + status);
+    console.log('header:', headers);
+    console.log('config:' + JSON.stringify(config));
+  })
+  .error(function(data, status, headers, config) {
+    console.log('daata:' + JSON.stringify(data));
+    console.log('status:' + status);
+    console.log('header:', headers);
+    console.log('config:' + JSON.stringify(config));
+  });
+
   this.work = true;
   this.blog = false;
 
   this.tes = 'test';
+  this.contents = [];
+
+  for(var i = 0; i < 100; i++){
+    var content = new Content('Title' + i, '');
+    this.contents.push(content);
+  }
 
   console.log('IN THE MAIN');
 
@@ -22,4 +54,4 @@ function MainCtrl() {
   };
 }
 
-angular.module('yoAngularApp').controller('MainCtrl', MainCtrl);
+angular.module('yoAngularApp').controller('MainCtrl',['$http', MainCtrl]);
